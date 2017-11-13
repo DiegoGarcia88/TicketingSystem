7f870fafe397d16b2cefe0c370d78a5757d63130
DiegoGarcia88 <diegogarcia19188@gmail.com>
2017-11-13 16:37:31 -0300

    added action methods and some folders

---
+++ src/TicketingSystem/Controllers/TicketingController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace TicketingSystem.Controllers
+{
+    public class TicketingController : Controller
+    {
+        public void Register()
+        {
+
+        }
+        public void Login()
+        {
+
+        }
+
+        public void DashBoard()
+        {
+
+        }
+
+        public void Create()
+        {
+
+        }
+
+        public void Edit()
+        {
+
+        }
+    }
+}
